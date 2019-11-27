a19b287fe9903428d19bebb2430a0032f37de69f
thunder775 <rahulmasih61@gmail.com>
2019-11-27 14:25:58 +0530

    done

--- bin/main.dart
+++ bin/main.dart
@@ -11,6 +11,36 @@
 //    [3,1,2],
 //    [3,2,1]
 //  ]
+void addToList(List<int> numbers,List<List<int>> temp){
+  List<int> tempo = [];
+  numbers.forEach((integer) {
+    tempo.add(integer);
+  });
+  temp.add(tempo);
+}
+
+void swap(List<dynamic> list, int i, int j) {
+  int temp = list[i];
+  list[i] = list[j];
+  list[j] = temp;
+}
+
+List<List<int>> permute(List<int> numbers, int start) {
+  List<List<int>> temp = [];
+  if (start == numbers.length-1) {
+   addToList(numbers, temp);
+  return temp;
+  }
+  for (int i = start; i < numbers.length; i++) {
+    swap(numbers, start, i);
+    temp.addAll(permute(numbers, start + 1));
+    swap(numbers, start, i);
+  }
+  return temp;
+}
+
+
 
 main() {
+  print(permute([1, 2, 3,4], 0));
 }
